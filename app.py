b3dbd725ae9067731ce1fcf104050be6eea3662d
kh.cho <kh.cho@monolith.co.kr>
2021-12-15 11:55:25 +0900

    add to scroll exporter command about large index

--- app.py
+++ app.py
@@ -89,8 +89,61 @@ def importer(host, index, file, verbose):
     print("Done")
 
 
+@click.command(help='export(scroll) json format from elasticsearch')
+@click.option('--host', '-h', type=click.STRING, required=True, default=ES_URL, help='host of elasticsearch(include port number)')
+@click.option('--index', '-i', type=click.STRING, required=True, help='index name')
+@click.option('--file', '-f', type=click.STRING, help='ouput filename')
+@click.option('--verbose', '-v', is_flag=True, help='verbose message')
+def scroll_exporter(host, index, file, verbose):
+    print('Elastic Hunter')
+    start_dt = datetime.now()
+    es = Elasticsearch(host)
+    res = es.count(index=index)
+    count = res['count']
+    print('host name: {}'.format(host))
+    print('index name: {}'.format(index))
+    if verbose:
+        pprint(es)
+        pprint(es.info())
+    if not file:
+        file = '{}.json'.format(index)
+    with open(file, 'w') as fp:
+        resp = es.search(
+            index=index,
+            body={
+                'size': 10000,
+                'query': {
+                    'match_all': {}
+                },
+                'sort': [
+                    '_doc'
+                ]
+            },
+            scroll = '1m' 
+        )
+        old_scroll_id = resp['_scroll_id']
+        for doc in resp['hits']['hits']:
+            fp.write('%s\n' % json.dumps(doc['_source']))
+        while len(resp['hits']['hits']):
+            resp = es.scroll(
+                scroll_id = old_scroll_id,
+                scroll = '1m'
+            )
+            old_scroll_id = resp['_scroll_id']
+            for doc in resp['hits']['hits']:
+                fp.write('%s\n' % json.dumps(doc['_source']))
+    print('count of document on index: {}'.format(count))
+    print('scroll_id: {}'.format(old_scroll_id))
+    es.clear_scroll(scroll_id='_all')
+    es.close()
+    finish_dt = datetime.now()
+    print('elaspsed time: {}'.format(finish_dt-start_dt))
+    print("Done")
+
+
 cli.add_command(exporter)
 cli.add_command(importer)
+cli.add_command(scroll_exporter)
 
 
 if __name__=='__main__':
